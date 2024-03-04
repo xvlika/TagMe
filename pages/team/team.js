d13f9733ce81d8a1fe0533939caf68bd4d6cff9e
xvlika <shengeliagiorgi114@gmail.com>
2024-03-04 12:29:52 +0400

    changed design of team members page

--- pages/team/team.js
+++ pages/team/team.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const members = document.querySelectorAll('.member');
 
     members.forEach(member => {
-        member.addEventListener('click', function(event) {
+        member.addEventListener('mouseover', function(event) {
             members.forEach(m => m.classList.remove('enlarged'));
             this.classList.add('enlarged');
             event.stopPropagation();
         });
     });
 
-    document.body.addEventListener('click', function(event) {
+    document.body.addEventListener('mouseover', function(event) {
         if (!event.target.closest('.member')) {
             members.forEach(m => m.classList.remove('enlarged'));
         }
