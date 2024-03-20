c7e9ce6d4ce6678b0bb2d00d9d73730d3c3dc4cf
xvlika <shengeliagiorgi114@gmail.com>
2024-03-20 15:24:02 +0400

    yvelaze seksi

--- box.js
+++ box.js
@@ -14,10 +14,12 @@ expandedElements.forEach(function(expandedElement) {
 element.classList.toggle('expanded');
 
 if (element.classList.contains('expanded')) {
+    const boxImg = document.querySelector('.boxImg')
     element.style.display = 'flex';
     element.style.alignItems = 'center';
     element.style.flexDirection = 'row';
     element.style.justifyContent = 'space-between';
+    //boxImg.style.height = '20px'; 
     var pTag = document.createElement('p');
     pTag.textContent = expandedText;
     element.appendChild(pTag);
@@ -25,6 +27,7 @@ if (element.classList.contains('expanded')) {
     var pTags = element.getElementsByTagName('p');
     if (pTags.length > 0) {
         element.removeChild(pTags[0]);
+        boxImg.style.height = '30%'; 
     }
 }
 }
